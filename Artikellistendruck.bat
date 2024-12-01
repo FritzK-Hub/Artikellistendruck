fb28ebc15848a69cb0e4ea78dfc486f7ba0dd42e
Kevin Fritz <47331822+FritzK-Hub@users.noreply.github.com>
2024-12-01 05:00:21 +0100

    working barcodes

--- Artikellistendruck.bat
+++ Artikellistendruck.bat
@@ -1,2 +1,3 @@
 @echo off
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0HelperScript.ps1" %1 %~dp0
+pause
--- HelperScript.ps1
+++ HelperScript.ps1
@@ -2,11 +2,135 @@ param (
     [string]$filePath,
 	[string]$installPath
 )
+
+$Code128Spaces = @(
+	@(2,1,2,2,2,2),
+	@(2,2,2,1,2,2),
+	@(2,2,2,2,2,1),
+	@(1,2,1,2,2,3),
+	@(1,2,1,3,2,2),
+	@(1,3,1,2,2,2),
+	@(1,2,2,2,1,3),
+	@(1,2,2,3,1,2),
+	@(1,3,2,2,1,2),
+	@(2,2,1,2,1,3),
+	@(2,2,1,3,1,2),
+	@(2,3,1,2,1,2),
+	@(1,1,2,2,3,2),
+	@(1,2,2,1,3,2),
+	@(1,2,2,2,3,1),
+	@(1,1,3,2,2,2),
+	@(1,2,3,1,2,2),
+	@(1,2,3,2,2,1),
+	@(2,2,3,2,1,1),
+	@(2,2,1,1,3,2),
+	@(2,2,1,2,3,1),
+	@(2,1,3,2,1,2),
+	@(2,2,3,1,1,2),
+	@(3,1,2,1,3,1),
+	@(3,1,1,2,2,2),
+	@(3,2,1,1,2,2),
+	@(3,2,1,2,2,1),
+	@(3,1,2,2,1,2),
+	@(3,2,2,1,1,2),
+	@(3,2,2,2,1,1),
+	@(2,1,2,1,2,3),
+	@(2,1,2,3,2,1),
+	@(2,3,2,1,2,1),
+	@(1,1,1,3,2,3),
+	@(1,3,1,1,2,3),
+	@(1,3,1,3,2,1),
+	@(1,1,2,3,1,3),
+	@(1,3,2,1,1,3),
+	@(1,3,2,3,1,1),
+	@(2,1,1,3,1,3),
+	@(2,3,1,1,1,3),
+	@(2,3,1,3,1,1),
+	@(1,1,2,1,3,3),
+	@(1,1,2,3,3,1),
+	@(1,3,2,1,3,1),
+	@(1,1,3,1,2,3),
+	@(1,1,3,3,2,1),
+	@(1,3,3,1,2,1),
+	@(3,1,3,1,2,1),
+	@(2,1,1,3,3,1),
+	@(2,3,1,1,3,1),
+	@(2,1,3,1,1,3),
+	@(2,1,3,3,1,1),
+	@(2,1,3,1,3,1),
+	@(3,1,1,1,2,3),
+	@(3,1,1,3,2,1),
+	@(3,3,1,1,2,1),
+	@(3,1,2,1,1,3),
+	@(3,1,2,3,1,1),
+	@(3,3,2,1,1,1),
+	@(3,1,4,1,1,1),
+	@(2,2,1,4,1,1),
+	@(4,3,1,1,1,1),
+	@(1,1,1,2,2,4),
+	@(1,1,1,4,2,2),
+	@(1,2,1,1,2,4),
+	@(1,2,1,4,2,1),
+	@(1,4,1,1,2,2),
+	@(1,4,1,2,2,1),
+	@(1,1,2,2,1,4),
+	@(1,1,2,4,1,2),
+	@(1,2,2,1,1,4),
+	@(1,2,2,4,1,1),
+	@(1,4,2,1,1,2),
+	@(1,4,2,2,1,1),
+	@(2,4,1,2,1,1),
+	@(2,2,1,1,1,4),
+	@(4,1,3,1,1,1),
+	@(2,4,1,1,1,2),
+	@(1,3,4,1,1,1),
+	@(1,1,1,2,4,2),
+	@(1,2,1,1,4,2),
+	@(1,2,1,2,4,1),
+	@(1,1,4,2,1,2),
+	@(1,2,4,1,1,2),
+	@(1,2,4,2,1,1),
+	@(4,1,1,2,1,2),
+	@(4,2,1,1,1,2),
+	@(4,2,1,2,1,1),
+	@(2,1,2,1,4,1),
+	@(2,1,4,1,2,1),
+	@(4,1,2,1,2,1),
+	@(1,1,1,1,4,3),
+	@(1,1,1,3,4,1),
+	@(1,3,1,1,4,1),
+	@(1,1,4,1,1,3),
+	@(1,1,4,3,1,1),
+	@(4,1,1,1,1,3),
+	@(4,1,1,3,1,1),
+	@(1,1,3,1,4,1),
+	@(1,1,4,1,3,1),
+	@(3,1,1,1,4,1),
+	@(4,1,1,1,3,1),
+	@(2,1,1,4,1,2),
+	@(2,1,1,2,1,4),
+	@(2,1,1,2,3,2),
+	@(2,3,3,1,1,1)
+)
 $tmpFileName = "temp.xml"
 $layoutFileName = "EtikettenLayouts.bcfp"
 $xmlFile = "$installPath$tmpFileName"
 $layoutFile = "$installPath$layoutFileName"
 $command = "C:\Program Files (x86)\Barcode Forge\bcf.exe"
+
+function Append-Svg {
+	param (
+		[string]$width,
+		[string]$pos
+	)
+	$out = "`n"
+	$out += '<rect width="'
+	$out += $width 
+	$out += '" height="100" x="'
+	$out += $pos
+	$out += '" y="0" fill="black" />'
+	$out
+}
 
 
 function Append-Xml {
@@ -15,90 +139,59 @@ function Append-Xml {
 	)
 	$content | Out-File -FilePath $xmlFile -Append -NoNewline -Encoding utf8
 }
-
 function Clubber-Xml {
 	param (
 		[string]$content
 	)
 	$content | Out-File -FilePath $xmlFile -NoNewline -Encoding utf8
 }
+$GTIN = '9002701143629'
 
-function Append-Xml-Header {
-	Clubber-Xml '<?xml version="1.0" encoding="UTF-8"?><printjob labelsperrow="1" printdirection="vertical" startpositionx="1" startpositiony="1" designno="'
-	Append-Xml $layout
-	Append-Xml '"><printservice name="Microsoft Print to PDF"/>'
+$svgOut = '<svg id="Barcode" width="100%" height="100%" viewBox="0 0 123 100" preserveAspectRatio="none"> xmlns="http://www.w3.org/2000/svg">'
+$offset = 0
+
+$checksum = 105;
+$checkmultiplier = 1;
+for ($i = 0; $i -lt 6; $i += 2) {
+	$svgOut += Append-Svg $Code128Spaces[105][$i] $offset
+	$offset += $Code128Spaces[105][$i + 1] + $Code128Spaces[105][$i]
 }
-
-function Append-Xml-Block {
-	param (
-		[string]$name,
-		[string]$inner,
-		[string]$line
-	)
-	Append-Xml "<$name>"
-	$tokens = $line -split ";"
-	foreach ($token in $tokens) {
-		Append-Xml "<$inner>"
-		Append-Xml $token
-		Append-Xml "</$inner>"
+foreach ($item in 0..5) {
+	$currentValue = [int]($GTIN.Substring($item*2, 2))
+	$offsetTotal = ($item * 11 + 11)
+	$offsetCurrent = 0
+	$checksum += $currentValue * $checkmultiplier
+	$checkmultiplier += 1
+	for ($i = 0; $i -lt 6; $i += 2) {
+		$svgOut += Append-Svg $Code128Spaces[$currentValue][$i] ($offsetTotal + $offsetCurrent)
+		$offsetCurrent += $Code128Spaces[$currentValue][$i] + $Code128Spaces[$currentValue][$i+1]
 	}
-	Append-Xml "</$name>"
 }
-
-function Print {
-	param (
-		[string]$layout
-	)
-	[void]$MainForm.Dispose()
-	
-	Append-Xml-Header
-	
-	$counter = 0
-	Get-Content -Path $filePath | ForEach-Object {
-		$line = $_
-		if ($counter++ -eq 0) {
-			Append-Xml-Block "fieldmap" "field" $line
-			Append-Xml '<data>'
-			return
-		} else {
-			Append-Xml-Block "row" "cell" $line
-		}
-	}
-	Append-Xml '</data></printjob>'
-
-	& "$command" $layoutFile $xmlFile
-	Return
+$offset = 77 
+$checksum += 100 * $checkmultiplier
+$checkmultiplier += 1
+for ($i = 0; $i -lt 6; $i += 2) {
+	$svgOut += Append-Svg $Code128Spaces[100][$i] $offset
+	$offset += $Code128Spaces[100][$i + 1] + $Code128Spaces[100][$i]
 }
-
-
-# Setup GUI
-Add-Type -AssemblyName System.Windows.Forms
-$MainForm                    = New-Object system.Windows.Forms.Form
-$MainForm.ClientSize         = '512,92'
-$MainForm.text               = "Artikelliste Drucken"
-$MainForm.BackColor          = "#ffffff"
-
-$RegalBtn                   = New-Object system.Windows.Forms.Button
-$RegalBtn.BackColor         = "#3c3c3b"
-$RegalBtn.text              = "Regaletiketten"
-$RegalBtn.width             = 200
-$RegalBtn.height            = 80
-$RegalBtn.location          = New-Object System.Drawing.Point(28,6)
-$RegalBtn.Font              = 'Microsoft Sans Serif,18'
-$RegalBtn.ForeColor         = "#ffffff"
-$RegalBtn.Add_Click({Print "1"})
-
-$HangBtn                   = New-Object system.Windows.Forms.Button
-$HangBtn.BackColor         = "#3c3c3b"
-$HangBtn.text              = "Haengeetiketten"
-$HangBtn.width             = 200
-$HangBtn.height            = 80
-$HangBtn.location          = New-Object System.Drawing.Point(274,6)
-$HangBtn.Font              = 'Microsoft Sans Serif,18'
-$HangBtn.ForeColor         = "#ffffff"
-$HangBtn.Add_Click({Print "2"})
-
-$MainForm.Controls.Add($RegalBtn)
-$MainForm.Controls.Add($HangBtn)
-
-[void]$MainForm.ShowDialog()
+$offset = 88
+$value = [int]$GTIN.Substring(12,1) + 16
+$checksum += $value * $checkmultiplier
+$checksum = $checksum % 103
+for ($i = 0; $i -lt 6; $i += 2) {
+	$svgOut += Append-Svg $Code128Spaces[$value][$i] $offset
+	$offset += $Code128Spaces[$value][$i + 1] + $Code128Spaces[$value][$i]
+}
+$offset = 99 
+for ($i = 0; $i -lt 6; $i += 2) {
+	$svgOut += Append-Svg $Code128Spaces[$checksum][$i] $offset
+	$offset += $Code128Spaces[$checksum][$i + 1] + $Code128Spaces[$checksum][$i]
+}
+$offset = 110
+for ($i = 0; $i -lt 6; $i += 2) {
+	$svgOut += Append-Svg $Code128Spaces[106][$i] $offset
+	$offset += $Code128Spaces[106][$i + 1] + $Code128Spaces[106][$i]
+}
+$svgOut += Append-Svg 2 121
+$svgOut += '</svg>'
+Clubber-Xml $svgOut
