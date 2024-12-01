08de6f19b518f5b9b8fe4c14d65ea8ed521ac9a1
Kevin Fritz <47331822+FritzK-Hub@users.noreply.github.com>
2024-12-02 00:03:55 +0100

    Cleanup

--- HelperScript.ps1
+++ HelperScript.ps1
@@ -3,122 +3,15 @@ param (
 	[string]$installPath
 )
 
-$Code128Spaces = @(
-	@(2,1,2,2,2,2),
-	@(2,2,2,1,2,2),
-	@(2,2,2,2,2,1),
-	@(1,2,1,2,2,3),
-	@(1,2,1,3,2,2),
-	@(1,3,1,2,2,2),
-	@(1,2,2,2,1,3),
-	@(1,2,2,3,1,2),
-	@(1,3,2,2,1,2),
-	@(2,2,1,2,1,3),
-	@(2,2,1,3,1,2),
-	@(2,3,1,2,1,2),
-	@(1,1,2,2,3,2),
-	@(1,2,2,1,3,2),
-	@(1,2,2,2,3,1),
-	@(1,1,3,2,2,2),
-	@(1,2,3,1,2,2),
-	@(1,2,3,2,2,1),
-	@(2,2,3,2,1,1),
-	@(2,2,1,1,3,2),
-	@(2,2,1,2,3,1),
-	@(2,1,3,2,1,2),
-	@(2,2,3,1,1,2),
-	@(3,1,2,1,3,1),
-	@(3,1,1,2,2,2),
-	@(3,2,1,1,2,2),
-	@(3,2,1,2,2,1),
-	@(3,1,2,2,1,2),
-	@(3,2,2,1,1,2),
-	@(3,2,2,2,1,1),
-	@(2,1,2,1,2,3),
-	@(2,1,2,3,2,1),
-	@(2,3,2,1,2,1),
-	@(1,1,1,3,2,3),
-	@(1,3,1,1,2,3),
-	@(1,3,1,3,2,1),
-	@(1,1,2,3,1,3),
-	@(1,3,2,1,1,3),
-	@(1,3,2,3,1,1),
-	@(2,1,1,3,1,3),
-	@(2,3,1,1,1,3),
-	@(2,3,1,3,1,1),
-	@(1,1,2,1,3,3),
-	@(1,1,2,3,3,1),
-	@(1,3,2,1,3,1),
-	@(1,1,3,1,2,3),
-	@(1,1,3,3,2,1),
-	@(1,3,3,1,2,1),
-	@(3,1,3,1,2,1),
-	@(2,1,1,3,3,1),
-	@(2,3,1,1,3,1),
-	@(2,1,3,1,1,3),
-	@(2,1,3,3,1,1),
-	@(2,1,3,1,3,1),
-	@(3,1,1,1,2,3),
-	@(3,1,1,3,2,1),
-	@(3,3,1,1,2,1),
-	@(3,1,2,1,1,3),
-	@(3,1,2,3,1,1),
-	@(3,3,2,1,1,1),
-	@(3,1,4,1,1,1),
-	@(2,2,1,4,1,1),
-	@(4,3,1,1,1,1),
-	@(1,1,1,2,2,4),
-	@(1,1,1,4,2,2),
-	@(1,2,1,1,2,4),
-	@(1,2,1,4,2,1),
-	@(1,4,1,1,2,2),
-	@(1,4,1,2,2,1),
-	@(1,1,2,2,1,4),
-	@(1,1,2,4,1,2),
-	@(1,2,2,1,1,4),
-	@(1,2,2,4,1,1),
-	@(1,4,2,1,1,2),
-	@(1,4,2,2,1,1),
-	@(2,4,1,2,1,1),
-	@(2,2,1,1,1,4),
-	@(4,1,3,1,1,1),
-	@(2,4,1,1,1,2),
-	@(1,3,4,1,1,1),
-	@(1,1,1,2,4,2),
-	@(1,2,1,1,4,2),
-	@(1,2,1,2,4,1),
-	@(1,1,4,2,1,2),
-	@(1,2,4,1,1,2),
-	@(1,2,4,2,1,1),
-	@(4,1,1,2,1,2),
-	@(4,2,1,1,1,2),
-	@(4,2,1,2,1,1),
-	@(2,1,2,1,4,1),
-	@(2,1,4,1,2,1),
-	@(4,1,2,1,2,1),
-	@(1,1,1,1,4,3),
-	@(1,1,1,3,4,1),
-	@(1,3,1,1,4,1),
-	@(1,1,4,1,1,3),
-	@(1,1,4,3,1,1),
-	@(4,1,1,1,1,3),
-	@(4,1,1,3,1,1),
-	@(1,1,3,1,4,1),
-	@(1,1,4,1,3,1),
-	@(3,1,1,1,4,1),
-	@(4,1,1,1,3,1),
-	@(2,1,1,4,1,2),
-	@(2,1,1,2,1,4),
-	@(2,1,1,2,3,2),
-	@(2,3,3,1,1,1)
-)
-$tmpFileName = "temp.xml"
-$layoutFileName = "EtikettenLayouts.bcfp"
-$xmlFile = "$installPath$tmpFileName"
-$layoutFile = "$installPath$layoutFileName"
-$command = "C:\Program Files (x86)\Barcode Forge\bcf.exe"
+$Code128Spaces = . $PSScriptRoot\Code128Array.ps1
 
-function Append-Svg {
+$tmpFileName = "temp.xml"
+#$layoutFileName = "EtikettenLayouts.bcfp"
+$xmlFile = "$installPath$tmpFileName"
+#$layoutFile = "$installPath$layoutFileName"
+#$command = "C:\Program Files (x86)\Barcode Forge\bcf.exe"
+
+function Add-Svg {
 	param (
 		[string]$width,
 		[string]$pos
@@ -138,7 +31,7 @@ $c128ToB = 100;
 $c128stop = 106;
 $checksum = 105;
 $checkmultiplier = 1;
-function Concat-Svg {
+function Add-Code128 {
 	param (
 		[int]$value,
 		[int]$pos
@@ -148,25 +41,25 @@ function Concat-Svg {
 	$checkmultiplier += 1
 	$out = ""
 	for ($i = 0; $i -lt 6; $i += 2) {
-		$out += Append-Svg $Code128Spaces[$value][$i] $offset
+		$out += Add-Svg $Code128Spaces[$value][$i] $offset
 		$offset += $Code128Spaces[$value][$i + 1] + $Code128Spaces[$value][$i]
 	}
 	$out
 }
 $GTIN = '1234567089054'
 
-$svgOut += Concat-Svg $c128CStart 0
+$svgOut += Add-Code128 $c128CStart 0
 foreach ($item in 0..5) {
 	$currentValue = [int]($GTIN.Substring($item*2, 2))
 	$offsetTotal = ($item * 11 + 11)
 	$offsetCurrent = 0
-	$svgOut += Concat-Svg $currentValue ($offsetTotal + $offsetCurrent)
+	$svgOut += Add-Code128 $currentValue ($offsetTotal + $offsetCurrent)
 }
-$svgOut += Concat-Svg $c128ToB 77
+$svgOut += Add-Code128 $c128ToB 77
 $lastDigit = [int]$GTIN.Substring(12,1) + 16
-$svgOut += Concat-Svg $lastDigit 88 
-$svgOut += Concat-Svg $checksum 99
-$svgOut += Concat-Svg $c128stop 110
-$svgOut += Append-Svg 2 121
+$svgOut += Add-Code128 $lastDigit 88 
+$svgOut += Add-Code128 $checksum 99
+$svgOut += Add-Code128 $c128stop 110
+$svgOut += Add-Svg 2 121
 $svgOut += '</svg>'
 $svgOut | Out-File -FilePath $xmlFile -NoNewline -Encoding utf8
