5fd541be0376d7fcbe4087b3646ea7aa21b60ed1
Kevin Fritz <47331822+FritzK-Hub@users.noreply.github.com>
2025-04-02 11:19:16 +0200

    final final touches

--- Artikellistendrucken.bat
+++ Artikellistendrucken.bat
@@ -1,6 +1,5 @@
 @echo off
 
-
-copy "%~dp0Interface\Head.html" + %1 "%~dp0Interface\Gui.html"
+copy "%~dp0Interface\Head.html" + %1 "%~dp0Interface\Gui.html" >nul
 echo ^</p^>^<p id=^"file-name^" class=^"hide^"^>%~n1^</p^>^</body^>^</html^> >> "%~dp0Interface\Gui.html"
 start "" "%~dp0Interface\Gui.html"
